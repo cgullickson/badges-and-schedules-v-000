c0c37dd7d698cf4b4140e7737a6f7b7a7e5ac9dd
Chris Gullickson <cgullickson5@gmail.com>
2017-11-21 17:14:48 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,8 +13,8 @@ end
 
 def assign_rooms (names)
   room_assignments = []
-  names.each do |name|
-    room_assignments << ("Hello, #{name}! You'll be assigned to room #{each_with_index}")
+  names.each_with_index do |name,index|
+    room_assignments << ("Hello, #{name}! You'll be assigned to room #{index + 1}")
   end
   room_assignments
 end
