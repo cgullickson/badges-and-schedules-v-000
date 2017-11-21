f32ccd760891757a42ede3d4adf09813e02142af
Chris Gullickson <cgullickson5@gmail.com>
2017-11-21 17:13:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def badge_maker (name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (names)
+  badge_messages = []
+  names.each do |name|
+    badge_messages << ("Hello, my name is #{name}.")
+  end
+  badge_messages
+end
+
+def assign_rooms (names)
+  room_assignments = []
+  names.each do |name|
+    room_assignments << ("Hello, #{name}! You'll be assigned to room #{names.index + 1 }")
+  end
+  room_assignments
+end
